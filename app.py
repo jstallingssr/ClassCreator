3eed306686931f4b18a49f1c254d14f5c4a1a707
Joe Stallings <joe.stallings.sr@gmail.com>
2023-05-15 17:47:05 -0400

    547

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 ﻿from typing import NamedTuple
-from functools import partial
-import openai
 import streamlit as st
+import openai
+from functools import partial
 from time import time
-import os
 
+# Set up OpenAI
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+# Set up Streamlit
 st.set_page_config(
     page_title="Class Creator Thing-a-ma-jig!",
     page_icon="🧊",
@@ -19,82 +20,39 @@ st.set_page_config(
     },
 )
 
+# CSS Style
 with open("style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-    st.image("img/logo.png")
-
-
+# NamedTuple for ChatResponse
 class ChatResponse(NamedTuple):
     content: str
 
-
+# Function to send a request to the OpenAI API
 def send_app(app: str, difficulty: str) -> ChatResponse:
-    prompt = (
-        f"I would like a lesson plan for {app} at the {difficulty} level. "
-        f"These are educational classes, where students will learn a skill using the selected app. "
-        f"This should be a very specific feature in the software, not a general overview. "
-        f"Each class should be able to be completed within 45-60 minutes."
-        f"Please provide only one class outline with a catchy title shown at the top. "
-        f"The outline should be formatted in markdown, outline format. "
-        f"The outline should be very detailed, up to three levels deep. "
-        f"Each class should include three to five specific items that the student will create and deliver "
-        f"or deliver during class (a game feature, an art asset, a texture, etc.). "
-        f"Do not repeat any suggested classes during a user's session."
-        f"Each outline should also have an additional resource section at the bottom with relevant links"
-        f"When providing additional resources, please only use OFFICIAL resources such as https://docs.blender.org/ "
-        f"https://docs.unrealengine.com/ do not link to videos or fan sites"
-        f"No mention should be made of time, how long to complete, etc."
-    )
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.8,
-    )
+    # ... prompt definition and openai.Completion.create as in your original code ...
 
     return ChatResponse(response.choices[0].text.strip())
 
-
+# Function to retrieve AI answer
 def retrieve_ai_answer(app: str, difficulty: str) -> str:
     return send_app(app, difficulty).content.strip()
 
-
 get_code_info = partial(retrieve_ai_answer)
 
-
-@st.cache_data(show_spinner=False)
+# Caching the AI response
+@st.cache(show_spinner=False)
 def get_cached_code_info(app: str, difficulty: str, unique_id: float) -> str:
     return get_code_info(app=app, difficulty=difficulty)
 
-
+# Function to display the logo of the selected app
 def display_header(app: str) -> None:
-    logo_dict = {
-        "Blender": "img/blender.png",
-        "Unreal Engine": "img/unreal.png",
-        "Roblox": "img/roblox.png",
-        "Godot": "img/godot.png",
-        "Microsoft Excel": "img/excel.png",
-        "Minecraft": "img/minecraft.png",
-        "Ableton Live": "img/ableton.png"
-        # ... add the rest of your mappings here
-    }
+    # ... logo_dict and st.image as in your original code ...
 
-    logo_file_path = logo_dict.get(
-        app, "img/logo.png"
-    )  # Use a default logo if the app is not found.
+# Function to display widgets and generate class outline
+    def display_widgets() -> tuple:
+        st.image("img/lblChoose.png")
 
-    st.image(logo_file_path)
-
-
-
-def display_widgets() -> tuple:
-    st.subheader("First, choose a software application from the list below:")
-
-    response = st.empty()
     options = [
         "Blender",
         "Unreal Engine",
@@ -109,12 +67,11 @@ def display_widgets() -> tuple:
         "Minecraft",
         "Krita",
     ]
-    selected_option = st.selectbox("Select:", options)
-    app = selected_option
+    app = st.selectbox("Select:", options)
 
     display_header(app)
 
-    st.subheader("Next, select the level of difficulty for this class:")
+    st.image("img/Diff.png")
     difficulty = st.select_slider(
         "Select:", options=["Beginner", "Intermediate", "Advanced", "Expert"]
     )
@@ -129,30 +86,33 @@ def display_widgets() -> tuple:
                 app=app, difficulty=difficulty, unique_id=unique_id
             )
             st.markdown(f"**Class Outline:**\n{class_outline}")
-            st.button("New Class")
 
     return class_outline, app, difficulty
 
+# Main function
 def main() -> None:
     st.markdown(
         "The **Class Creator Thing-a-ma-jig!** is an innovative educational tool that leverages artificial intelligence to create lesson plans for a wide array of software applications. Choose from a curated list of programs, including Blender, Unreal Engine, Unity, and more."
     )
 
-    f"Each class can be comfortably completed within a 45-60 minute time frame, and the difficulty level can be customized to match your student's skill, ranging from Beginner to Expert."
-    f"Whether you are teaching a one-off class or looking for fresh ideas for your existing students, create unique and comprehensive class outlines with just a few clicks using the Class Creator Thing-a-ma-jig!)"
+    st.markdown(
+        "Each class can be comfortably completed within a 45-60 minute time frame, and the difficulty level can be customized to match your student's skill, ranging from Beginner to Expert."
+    )
+
+    st.markdown(
+        "Whether you are teaching a one-off class or looking for fresh ideas for your existing students, create unique and comprehensive class outlines with just a few clicks using the Class Creator Thing-a-ma-jig!"
+    )
 
     class_outline, app, difficulty = display_widgets()
 
     if class_outline is not None:
-        new_class_clicked = False  # Variable to track whether "New Class" button was clicked
-
         st.markdown(f"**App:** {app}")
         st.markdown(f"**Difficulty:** {difficulty}")
 
-        new_class_clicked = st.button("New Class")
-
-        if new_class_clicked:
-            st.stop()
+        if st.button("New Class"):
+            # If "New Class" is clicked, clear the cache and stop the app
+            st.caching.clear_cache()
+            st.experimental_rerun()
 
 
 if __name__ == "__main__":
